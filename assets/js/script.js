987b478123a9b58e37f2d9d941143f720b0ad6b1
saurabh-gaonkar <saurabhg@axioned.com>
2022-01-22 14:19:11 +0530

    js updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
     $(".hamburg-icon").click(function(){
         $(this).removeClass('hamburg-icon');
         $(this).addClass('cross-icon');
-    })
+    });
+$(".cross-icon").click(function(){
+    $(this).removeClass('cross-icon');
+    $(this).addClass('hamburg-icon');
+});
 })
 
 
