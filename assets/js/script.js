4bbced80e79e650120e5b5290c635d58c363ab73
saurabh-gaonkar <saurabhg@axioned.com>
2022-01-22 16:30:18 +0530

    javascript added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $(".btn3").css("background-color","white");
     $(".hamburg-icon").click(function(){
         $(this).removeClass('hamburg-icon');
         $(this).addClass('cross-icon');
@@ -6,7 +7,12 @@ $(document).ready(function(){
 $(".cross-icon").click(function(){
     $(this).removeClass('cross-icon');
     $(this).addClass('hamburg-icon');
+}); 
+$(".buttons span").click(function(){
+    $(".buttons span").css("background-color","inherit");
+    $(this).css("background-color","white");
 });
+
 })
 
 
